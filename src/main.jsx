f554512005d00392648085535b84ff799424180f
Luis Manzano <luissmanzanocha@gmail.com>
2026-02-13 18:33:47 -0600

    style: add separate mobile css file for responsive fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ErrorBoundary from './components/common/ErrorBoundary'
 import './index.css'
+import './mobile.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
